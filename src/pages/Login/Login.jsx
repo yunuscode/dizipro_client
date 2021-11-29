9c49a9afb347db2eaaf3015cfffbe1237de67d88
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-29 21:01:04 +0500

    add login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,17 @@
 import Header from "../../components/Header/Header";
 import "./Login.scss";
+import BeeImage from "../../assets/images/bee.png";
 
 export default function Login() {
 	return (
 		<div className="login">
 			<Header />
+			<section className="login__main-section">
+				<img className="login__bee-image" src={BeeImage} alt="" />
+				<form className="login__form">
+					<h2 className="login__title">Log in</h2>
+				</form>
+			</section>
 		</div>
 	);
 }
